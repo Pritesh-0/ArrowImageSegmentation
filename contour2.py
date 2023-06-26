c39237b1dbdafb696bb14b6f6a61c1f8d1597e1d
Pritesh-0 <priteshagrawal0@gmail.com>
2023-06-26 18:05:14 +0530

    added left and right images

--- contour2.py
+++ contour2.py
@@ -6,24 +6,28 @@ def increase_contrast(image):
                        [-1,  9, -1],
                        [-1, -1, -1]])
     high_pass = cv2.filter2D(image, -1, kernel)
-    #contrast = cv2.subtract(image, high_pass)
 
     blurred = cv2.GaussianBlur(high_pass, (7, 7), 3)
-    _, final_image = cv2.threshold(blurred, 70, 200, cv2.THRESH_BINARY)
+    _, final_image = cv2.threshold(blurred, 70, 255, cv2.THRESH_BINARY)
 
     return final_image
+    
+
 # Load the image
-image_path = 'test2.jpeg'
+image_path = 'test3.jpeg'
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 temp = cv2.imread('temp.jpeg', cv2.IMREAD_GRAYSCALE) 
+temp1 = cv2.imread('temp1.jpeg', cv2.IMREAD_GRAYSCALE) 
 image = increase_contrast(image)
-# Convert to grayscale
-#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+temp = increase_contrast(temp)
+temp1 = increase_contrast(temp1)
+
+
+
 # Thresholding
 _, binary = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
-_, tempbin = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
- 
-#binary=cv2.bitwise_not(binary)
+_, tempbin = cv2.threshold(temp1, 127, 255, cv2.THRESH_BINARY)
+
 
 # Contour detection
 contours1, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -36,12 +40,9 @@ for contour in contours1:
     area = cv2.contourArea(contour)
     x, y, w, h = cv2.boundingRect(contour)
     aspect_ratio = w / float(h)
-    
-    # Adjust the following conditions as per your requirements
-    if area > 1000  and area < 8000 and aspect_ratio > 1 and aspect_ratio < 4:
+    if area > 1000  and aspect_ratio > 1 and aspect_ratio < 4:
         filtered_contours.append(contour)
 print(len(filtered_contours))
-# Identify the arrow
 ret=[]
 for contour in filtered_contours:
 	rt = cv2.matchShapes(contour,mask,1,0.0)
